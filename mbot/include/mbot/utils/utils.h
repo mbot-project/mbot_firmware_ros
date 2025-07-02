506c988faa89838502939fae64ff846185b8942e
Shaw Sun <49962634+sxy-sun@users.noreply.github.com>
2025-07-02 18:26:44 -0400

    Added i2c bus recovery code (#8)
    * added i2c bus reset function
    
    * update for compile error

--- mbot/include/mbot/utils/utils.h
+++ mbot/include/mbot/utils/utils.h
@@ -4,6 +4,7 @@
 
 int mbot_init_i2c();
 int _mbot_init_i2c(unsigned int pico_sda_pin, unsigned int pico_scl_pin);
+void _i2c_bus_recover(unsigned int sda_pin, unsigned int scl_pin);
 int _check_i2c0_enabled();
 int validate_mbot_classic_FRAM_data(mbot_params_t* params, int mot_left, int mot_right, int mot_unused);
 int validate_mbot_omni_FRAM_data(mbot_params_t* params, int mot_left, int mot_right, int mot_back);
